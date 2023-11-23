cd39fc7ebf0baeb2fc22acb10272c01be58a8bf5
miksor2023 <miksor@yandex.ru>
2023-11-23 16:06:35 +0300

    Initial commit

---
+++ src/main/java/com/pro/sky/java/course2/homeworkemployeebook/Controller.java
@@ -0,0 +1,8 @@
+package com.pro.sky.java.course2.homeworkemployeebook;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+}
---
+++ src/main/java/com/pro/sky/java/course2/homeworkemployeebook/HomeworkEmployeebookApplication.java
@@ -0,0 +1,13 @@
+package com.pro.sky.java.course2.homeworkemployeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkEmployeebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkEmployeebookApplication.class, args);
+	}
+
+}
